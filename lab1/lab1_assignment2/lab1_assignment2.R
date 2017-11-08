98227890262ea6fd1499dfcfa91088faf707158a
Max Lund <maxlun@gmail.com>
2017-11-08 21:33:22 +0100

    set-up lab1/assignment2 for lab

--- lab1/lab1_assignment2/lab1_assignment2.R
+++ lab1/lab1_assignment2/lab1_assignment2.R
@@ -5,7 +5,7 @@ library(readxl)
 
 df_machines = read_excel("machines.xlsx")
 machines = data.matrix(df_machines)
-head(machines)
+#head(machines)
 
 probability=function(theta, x) {
   return (theta * exp(-theta * x))
@@ -15,4 +15,7 @@ log_likelihood=function(theta, x) {
   return (log(probability(theta, x)))
 }
 
-
+# TODO
+# fit theta (try different values of theta?) to data in machines
+# .. find a value for theta that maximizes the likelihood
+# of the data observed in machines

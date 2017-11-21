de549548cf3b92a0df4f20290ad217cf58a840bf
Max Lund <maxlun@gmail.com>
2017-11-21 17:14:59 +0100

    broken lab2/ass3

--- lab2/assignment3/states.R
+++ lab2/assignment3/states.R
@@ -1,4 +1,5 @@
 library(tree)
+library(boot)
 
 set.seed(12345)
 data = read.csv("State.csv", header = TRUE, 
@@ -27,4 +28,55 @@ summary(model.optimal)
 #*****
 # histogram of residuals
 hist(resid(model.optimal))
+
+# function that produces the statistics for the bootstrap function boot()
+boot.func.nonparam = function(data, index)
+{
+  sample = data[index, ]
+  model = tree(formula = EX ~ MET,
+               data = sample,
+               control = tree.control(nrow(data.ordered), minsize = 8))
+  model.pruned = prune.tree(model, best = best.size)
+  prediction = predict(model.pruned, newdata = data.ordered)
+  return (prediction)
+}
+
+boot.func.param = function(data)
+{
+  model = tree(formula = EX ~ MET,
+               data = data,
+               control = tree.control(nrow(data.ordered), minsize = 8))
+  model.pruned = prune.tree(model, best = best.size)
+  prediction = predict(model.pruned, newdata = data.ordered)
+  return (prediction)
+}
+
+boot.rng = function(data, mle)
+{
+  new_data = data.frame(EX = data$EX, MET = data$MET)
+  n = length(data$EX)
+  new_data$EX = rnorm(n, predict(mle, newdata = new_data), sd(mle$residuals))
+  return(new_data)
+}
+
+boot.nonparam = boot(data = data.ordered,
+                     statistic = boot.func.nonparam,
+                     R = 1000)
+plot(boot.nonparam)
+
+boot.param = boot(data = data.ordered,
+                  statistic = boot.func.param,
+                  R = 1000,
+                  mle = model.optimal,
+                  ran.gen = boot.rng,
+                  sim = "parametric")
+
+
+plot(boot.param)
+
+summary(model.optimal)
+
+# ******
+# TODO: plot confidence bands ... ??
+# ******
 

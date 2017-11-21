d9bf50e4e3547b9edf8c61fd47387bcc1dca8996
Max Lund <maxlun@gmail.com>
2017-11-21 21:29:07 +0100

    progress lab2/ass3

--- lab2/assignment3/states.R
+++ lab2/assignment3/states.R
@@ -6,10 +6,10 @@ data = read.csv("State.csv", header = TRUE,
                 sep = ";", dec = ",", fill = TRUE)
 
 # sort data by variable MET (desc)
-data.ordered = data[order(MET),]
+data.ordered = data[order(data$MET),]
 
 # plot EX vs MET
-plot(data.ordered$EX, data.ordered$MET)
+plot(data.ordered$MET, data.ordered$EX)
 # fit a tree model, #observations in leaf >= 8
 model = tree(formula = EX ~ MET,
              data = data.ordered,
@@ -23,60 +23,80 @@ best.size = model.cv$size[which(model.cv$dev==min(model.cv$dev))]
 model.optimal = prune.tree(model, best = best.size)
 # report the selected tree
 summary(model.optimal)
+
 #*****
-# TODO: plot original and fitted data.. ??
+# TODO: plot original and fitted data..
 #*****
+# like this?
+fitted_data = predict(model.optimal, newdata = data.ordered)
+plot(data.ordered$MET, data.ordered$EX, col = "green")
+points(data.ordered$MET, preds, col = "red")
+
 # histogram of residuals
 hist(resid(model.optimal))
 
-# function that produces the statistics for the bootstrap function boot()
-boot.func.nonparam = function(data, index)
+# function that produces the statistics for the nonparametric bootstrap function boot()
+nonparametric = function(data, index)
 {
   sample = data[index, ]
   model = tree(formula = EX ~ MET,
                data = sample,
-               control = tree.control(nrow(data.ordered), minsize = 8))
+               control = tree.control(nrow(sample), minsize = 8))
   model.pruned = prune.tree(model, best = best.size)
   prediction = predict(model.pruned, newdata = data.ordered)
   return (prediction)
 }
 
-boot.func.param = function(data)
+# same for parametric boostrapping
+parametric = function(data)
 {
   model = tree(formula = EX ~ MET,
                data = data,
-               control = tree.control(nrow(data.ordered), minsize = 8))
+               control = tree.control(nrow(data), minsize = 8))
   model.pruned = prune.tree(model, best = best.size)
   prediction = predict(model.pruned, newdata = data.ordered)
   return (prediction)
 }
 
-boot.rng = function(data, mle)
+# generate some new data for EX
+rng = function(data, mle)
 {
   new_data = data.frame(EX = data$EX, MET = data$MET)
   n = length(data$EX)
-  new_data$EX = rnorm(n, predict(mle, newdata = new_data), sd(mle$residuals))
+  new_data$EX = rnorm(n, predict(mle, newdata = data), sd(resid(mle)))
   return(new_data)
 }
 
 boot.nonparam = boot(data = data.ordered,
-                     statistic = boot.func.nonparam,
+                     statistic = nonparametric,
                      R = 1000)
-plot(boot.nonparam)
+boot.nonparam.cb = envelope(boot.nonparam, level = 0.95)
+
+#plot(boot.nonparam)
+
+# plot MET vs EX, predictions and
+# confidence bands for model's predictions
+plot(data.ordered$MET, data.ordered$EX, col = "green", main = "Confidence bands (non-parametric)")
+points(data.ordered$MET, preds, col = "red")
+lines(data.ordered$MET, boot.nonparam.cb$point[1, ])
+lines(data.ordered$MET, boot.nonparam.cb$point[2, ])
 
 boot.param = boot(data = data.ordered,
-                  statistic = boot.func.param,
+                  statistic = parametric,
                   R = 1000,
                   mle = model.optimal,
-                  ran.gen = boot.rng,
+                  ran.gen = rng,
                   sim = "parametric")
+boot.param.cb = envelope(boot.param, level = 0.95)
 
-
+# same for parametric bootstrapping
 plot(boot.param)
+plot(data.ordered$MET, data.ordered$EX, col = "green", main = "Confidence bands (parametric)")
+points(data.ordered$MET, preds, col = "red")
+lines(data.ordered$MET, boot.param.cb$point[1, ])
+lines(data.ordered$MET, boot.param.cb$point[2, ])
 
-summary(model.optimal)
-
-# ******
-# TODO: plot confidence bands ... ??
-# ******
+# ********************
+# TODO: prediction bands ..?
+# ********************
 
